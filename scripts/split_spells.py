ad01530b9b3c3c44a111e4199b9e884d10df7bf9
NilsvMulekom <nilmul@pc.localdomain>
2026-08-03 22:21:16 +0200

    split_spells: Added custom types

--- scripts/split_spells.py
+++ scripts/split_spells.py
@@ -1,4 +1,4 @@
-import logging
+from dataclasses import dataclass
 from pathlib import Path
 import shutil
 import re
@@ -7,6 +7,19 @@ import re
 # TODO: Automatic table forming
 # TODO: add metadata as obsidian metadata
 # TODO: Add scripts for classes and potentially other things
+
+@dataclass
+class SpellContent:
+    content: list[str]
+
+@dataclass
+class Spell:
+    name:    str
+    content: SpellContent
+
+@dataclass
+class SpellBook:
+    spells: dict[str, Spell]
 
 PATTERN_LIST = [
     "Blinded",
@@ -26,17 +39,18 @@ PATTERN_LIST = [
     "Unconscious",
 ]
 
-def print_spell_book(spell_book: dict[str, dict[str, list[str]]]):
+def print_spell_book(spell_book: SpellBook):
     for file_name, spell_dict in spell_book.items():
         print(f"filename: {file_name}")
         for index, line in enumerate(spell_dict["content"]):
             print(f"{index}: {line}")
             
-def write_files(spell_book: dict[str, dict[str, list[str]]], output_directory: str):
+def write_files(spell_book: SpellBook, output_directory: str):
     for file_name, spell_dict in spell_book.items():
         write_file(file_name, spell_dict["content"], output_directory)
             
 def write_file(spell_title: str, file_body: list[str], output_path: str):
+    # TODO: Change to take spells instead of separate title and body
     file_name: str = f"{spell_title}.md"
     output_file = Path(output_path) / file_name
     
@@ -52,23 +66,25 @@ def create_output_dir(output_directory: str):
 
     output_path.mkdir(exist_ok=True)
     
-def split_files(input_file) -> dict[str, dict[str, list[str]]]:
+def split_files(input_file) -> SpellBook:
     input_path  = Path(input_file)
     
     with open(input_path, "r", encoding="utf-8") as file:
         file_content = file.read()
 
-    spell_book: dict[str, dict[str, list[str]]] = {}
+    spell_book: SpellBook = {}
     first_file: bool = True
     
     for line in file_content.splitlines():
         if line.startswith("#### "):
             # After the first cycle, copy the last one into spell_book
             if (first_file == False):
-                spell_with_metadata : dict[str, list] = {}
+                spell_with_metadata : Spell = {}
                 spell_with_metadata["content"] = spell_body
+                print(spell_with_metadata)
                 spell_book[spell_name] = spell_with_metadata
-            spell_body: list[str] = []
+                print(f"spell_book: {spell_book}")
+            spell_body: SpellContent = []
             first_file = False
 
             # Slice from the 5th character onwards
@@ -77,17 +93,25 @@ def split_files(input_file) -> dict[str, dict[str, list[str]]]:
         else:
             spell_body.append(line)
     # Copy the last item into spell_book
+    print(spell_body)
     spell_with_metadata : dict[str, list]
     spell_with_metadata["content"] = spell_body
     spell_book[spell_name] = spell_with_metadata
+    
+    print("henk")
+    for file_name, spell_dict in spell_book.items():
+        print(f"filename: {file_name}")
+        for index, line in enumerate(spell_dict["content"]):
+            print(f"{index}: {line}")
+    # print(spell_book)
 
     return spell_book
 
-def add_linking(spell_book: dict[str, dict[str, list[str]]]) -> dict[str, dict[str, list[str]]]:      
-    linked_book: dict[str, dict[str, list[str]]] = {}
+def add_linking(spell_book: SpellBook) -> SpellBook:
+    linked_book: SpellBook = {}
 
     for file_name, spell_dict in spell_book.items():
-        spell_body: list[str] = []
+        spell_body: SpellContent = []
         for line in spell_dict["content"]:
             new_line = line
             for pattern in PATTERN_LIST:
@@ -98,10 +122,10 @@ def add_linking(spell_book: dict[str, dict[str, list[str]]]) -> dict[str, dict[s
     return linked_book
     
 def main(input_file, output_directory):
-    spell_book: dict[str, dict[str, list[str]]] = {}
+    spell_book: SpellBook = {}
     create_output_dir(output_directory)
     spell_book = split_files(input_file)
-    print_spell_book(spell_book)
+    # print_spell_book(spell_book)
     # write_files(spell_book, output_directory)
     # spell_book = add_linking(spell_book)
     # print_spell_book(spell_book)
